213c4bd1bc62d50c4ffd546a9e51eb66a20065d8
muhammadkhan123786 <muhammadkhan123829@gmail.com>
2026-03-13 12:38:13 +0500

    Deleted files

--- backend/src/schemas/domestic.customer.schema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-import { commonCustomerValidationSchema } from './corporate.customer.schema';
-
-export const domesticCustomerSchema = z.object({
-    ...commonCustomerValidationSchema,
-    customerType: z.literal("domestic"),
-
-});
